6690c5d3e96625d5c16862f223f5eabf1d936ea7
pannawatreehung <reehungpannwat@gmail.com>
2024-07-19 16:13:42 +0700

    วันแรกว้อย2

--- ep1/index.ts
+++ ep1/index.ts
@@ -6,3 +6,9 @@ var went_to_go_home : boolean = true
 console.log(firstname)
 console.log(y)
 console.log(went_to_go_home)
+
+const a :number = 7
+const b :number = 1
+console.log(a==b)
+console.log(a>b)
+console.log(a<=b)
