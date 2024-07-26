4df5bd8bcd7577f666ea0e0f931c0a8bff8c829f
pannawatreehung <reehungpannwat@gmail.com>
2024-07-26 14:21:22 +0700

    26กค67

---
+++ ep3/index.ts
@@ -0,0 +1,19 @@
+function helloWorld() {
+    console.log("สวัสดี")
+}
+
+helloWorld() //
+
+function helloName(name: string){
+    console.log(name)
+}
+
+helloName('First')
+
+function getPi() {
+    return 3.14
+}
+
+getPi()
+return 3.14
+console.log(getPi())
