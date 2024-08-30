548685f87e53ddcfdbe67f558c9f07b561b9087b
pannawatreehung <reehungpannwat@gmail.com>
2024-08-30 15:46:55 +0700

    .

--- ep7/index.ts
+++ ep7/index.ts
@@ -9,4 +9,35 @@ let avg = sum / score.length
 console.log("total; ", sum)
 console.log("avg; ", avg)
 
-const myStudents; string = "่เซน,แทน,เวฟ,รุ้ง,เท็ก,สร"
+const myStudents: string = "่เซน,แทน,เวฟ,รุ้ง,เท็ก,สร"
+
+const myStudentsArray : string[] = myStudents.split(',')
+
+console.log(myStudents)
+console.log(myStudentsArray)
+
+const animal: string = "่dog*cat*rabbit*bear"
+
+const animalArray : string[] = animal.split(',')
+
+
+const friendsName: string = "เต้, โต้, ต้า, ตุ๋ย"
+const friendsNameArray: string[] = friendsName.split(',')
+
+friendsNameArray.sort()
+
+console.log("เรียง ก - ฮ: ", friendsNameArray)
+
+friendsNameArray.reverse()
+
+console.log("เรียง ฮ - ก: ", friendsNameArray)
+
+const me = {
+    firstName: 'Pannawat',
+    lastName: 'Reehung',
+    age: 19,
+    gender: male,
+    phoneNumber: "0909768071",
+    address: 'Mukdahan Thailand',
+}
+console.log (me)
