fbb435e6f7749e7903d5108bef8b9d89f3bf5f8f
pannawatreehung <reehungpannwat@gmail.com>
2024-08-30 13:48:05 +0700

    .

---
+++ ep7/index.ts
@@ -0,0 +1,12 @@
+const score: number[] =[10,23,26,30,33]
+
+let sum: number = 0
+for (let i = 0; i < score.length; i++) {
+    sum = sum + score[i]
+}
+
+let avg = sum / score.length
+console.log("total; ", sum)
+console.log("avg; ", avg)
+
+const myStudents; string = "่เซน,แทน,เวฟ,รุ้ง,เท็ก,สร"
