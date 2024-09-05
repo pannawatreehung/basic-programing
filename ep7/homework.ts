0ae858723b7eed8fe7df488f0e1f4eea3a3178dd
pannawatreehung <reehungpannwat@gmail.com>
2024-09-06 01:03:10 +0700

    .

---
+++ ep7/homework.ts
@@ -0,0 +1,26 @@
+const me = {
+    firstName: 'Mr.Lee' ,
+    LastName : 'Hyung',
+    age : 20,
+    phoneNumber : 0356720439,
+    sex : 'male',
+    addres : 'THA',
+    city : 'PattayaCity',
+    numBerCity : 20027,
+    street :  'LaAmTong',
+    numberStreet : 27,
+    mail : 'NhongLEE.Hyung@gmail.com'
+}
+
+// ประวัติส่วนตัว
+
+// ชื่อ : Pannawat
+// นามสกุล : Lee
+// อายุ : 19
+//เบอร์โทร : 0196567777
+//เพศ : ชาย
+//ที่อยู่ : Lao
+//เมือง : ViangJan
+//ถนน : LhaoThong
+//ถนนหมายเลข : 3081
+//เมล :PeterLEE.2005@gmail.com
